51ef66ee7fa007b2be731ae62d8073c32e022ab9
Pierre Thierry <pierre@nothos.net>
2019-11-04 01:45:04 +0100

    Add beta-reduction, NOR, AOR

--- src/Main.elm
+++ src/Main.elm
@@ -38,6 +38,98 @@ y = let y0 = Lambda "x" <| Application (Variable "f") (Application (Variable "x"
         Lambda "f" <| Application y0 y0
 
 
+---- Beta reduction
+
+type Direction = Body | Fun | Arg
+type alias Path = List Direction
+
+dirChar dir =
+    case dir of
+        Body -> 'B'
+        Fun -> 'F'
+        Arg -> 'A'
+
+pathStr = String.fromList << List.map dirChar
+
+
+normalOrder : Expression -> Maybe Path
+normalOrder expr =
+    case expr of
+        Application (Lambda _ _) _ ->
+            Just []
+
+        Application fun arg ->
+            case normalOrder fun of
+                Just path ->
+                    Just <| Fun :: path
+
+                Nothing ->
+                    normalOrder arg |> Maybe.map ((::) Arg)
+
+        Lambda _ body ->
+            normalOrder body |> Maybe.map ((::) Body)
+
+        _ ->
+            Nothing
+
+applicativeOrder expr =
+    case expr of
+        Application fun arg ->
+            case applicativeOrder arg of
+                Just path ->
+                    Just <| Arg :: path
+
+                Nothing ->
+                    case fun of
+                        Lambda _ _ ->
+                            Just []
+
+                        _ ->
+                            applicativeOrder fun |> Maybe.map ((::) Fun)
+        Lambda _ body ->
+            applicativeOrder body |> Maybe.map ((::) Body)
+
+        _ ->
+            Nothing
+
+
+betaReduce : Expression -> Path -> Maybe Expression
+betaReduce expr path =
+    case (expr, path) of
+        (Application (Lambda var body) arg, []) ->
+            Just <| substitute var arg body
+
+        (Application fun arg, Fun :: path2) ->
+            betaReduce fun path2 |> Maybe.map (\f -> Application f arg)
+
+        (Application fun arg, Arg :: path2) ->
+            betaReduce arg path2 |> Maybe.map (Application fun)
+
+        (Lambda var body, Body :: path2) ->
+            betaReduce body path2 |> Maybe.map (Lambda var)
+
+        _ ->
+            Nothing
+
+
+substitute var value expr =
+    case expr of
+        Lambda var2 body ->
+                Lambda var2 (if var == var2 then
+                                 body
+                             else
+                                 substitute var value body)
+
+        Application fun arg ->
+            Application (substitute var value fun) (substitute var value arg)
+
+        Variable var2 ->
+            if var == var2 then
+                value
+            else
+                Variable var2
+
+
 ---- INIT ----
 
 init : ( Model, Cmd Msg )
