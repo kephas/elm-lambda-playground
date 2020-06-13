1f702f9195e8d21c4ffea478fe41fa682dd3a19f
Pierre Thierry <pierre@nothos.net>
2020-06-13 02:21:09 +0200

    Finish parser

--- src/Lambda/Parser.elm
+++ src/Lambda/Parser.elm
@@ -1,19 +1,25 @@
 module Lambda.Parser exposing (..)
 
 import Lambda.Calculus exposing (..)
+import Maybe.Extra exposing (unwrap)
 import Parser exposing (..)
+import Parser.Extras exposing (parens, some)
 import String exposing (contains, fromChar)
 
 
 specialChars =
     "\\. ()"
 
+
 mustSpaces =
-    let isSpace = (\c -> c == ' ')
+    let
+        isSpace =
+            \c -> c == ' '
     in
     succeed ()
         |. chompIf isSpace
         |. chompWhile isSpace
+
 
 isVarChar : Char -> Bool
 isVarChar char =
@@ -33,27 +39,59 @@ variable =
     map Variable varName
 
 
-lexpression = lazy (\_ -> expression)
+lexpression =
+    lazy (\_ -> expression)
+
 
 lambda : Parser Expression
 lambda =
     succeed Lambda
-        |. symbol "\\"
+        |. oneOf [ symbol "\\", symbol "λ" ]
         |= varName
         |. symbol "."
         |= lexpression
 
+
+term : Parser Expression
+term =
+    oneOf
+        [ parens lexpression
+        , lambda
+        , variable
+        ]
+
+
+buildApplicationChain : List Expression -> Maybe Expression
+buildApplicationChain terms =
+    case terms of
+        [ term1, term2 ] ->
+            Just <| Application term1 term2
+
+        term1 :: term2 :: term3 :: terms4_ ->
+            -- note that terms4_ might be []
+            buildApplicationChain <| Application term1 term2 :: term3 :: terms4_
+
+        _ ->
+            Nothing
+
+
+parseApplicationChain : List Expression -> Parser Expression
+parseApplicationChain terms =
+    terms
+        |> buildApplicationChain
+        |> unwrap (problem "not enough terms") succeed
+
+
 application : Parser Expression
 application =
-    succeed Application
-        |= backtrackable lexpression
-        |. mustSpaces
-        |= lexpression
+    sequence { start = "", separator = " ", end = "", spaces = succeed (), item = term, trailing = Forbidden }
+        |> andThen parseApplicationChain
+
 
 expression : Parser Expression
 expression =
     oneOf
-        [ application
-        , lambda
+        [ backtrackable <| application
+        , backtrackable <| lambda
         , variable
         ]
--- tests/Tests.elm
+++ tests/Tests.elm
@@ -1,10 +1,11 @@
 module Tests exposing (..)
 
-import Test exposing (..)
 import Expect
 import Lambda.Calculus exposing (..)
-import Parser as P
 import Lambda.Parser as LP
+import Parser as P
+import Test exposing (..)
+
 
 
 -- Check out https://package.elm-lang.org/packages/elm-explorations/test/latest to learn more about testing in Elm!
@@ -25,4 +26,17 @@ all =
         , test "Basic application" <|
             \_ ->
                 Expect.equal (Ok (Application (Variable "x") (Variable "y"))) <| P.run LP.expression "x y"
+
+        {- , test "Application chain building" <|
+           \_ ->
+               Expect.equal (Application (Application (Application (Variable "a") (Variable "b")) (Variable "c")) (Variable "d")) <|
+                   P.run
+                   LP.parseApplicationChain [ Variable "a", Variable "b", Variable "c", Variable "d" ]
+        -}
+        , test "Nested applications" <|
+            \_ ->
+                Expect.equal (Ok (Application (Application (Application (Variable "a") (Variable "b")) (Variable "c")) (Variable "d"))) <| P.run LP.expression "a b c d"
+        , test "Complex expression" <|
+            \_ ->
+                Expect.equal (Ok y) <| P.run LP.expression "λf.(λx.f (x x)) λx.f (x x)"
         ]
